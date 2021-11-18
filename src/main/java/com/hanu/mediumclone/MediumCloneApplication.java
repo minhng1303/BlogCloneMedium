5dcb5af6acd0af9095ee0dd2c3892a32abffdd67
Tien Minh <minhng130300@gmail.com>
2021-11-18 11:06:35 +0700

    Add jwt into the project

--- src/main/java/com/hanu/mediumclone/MediumCloneApplication.java
+++ src/main/java/com/hanu/mediumclone/MediumCloneApplication.java
@@ -3,9 +3,14 @@ package com.hanu.mediumclone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableJpaRepositories
+@Configuration
+@ComponentScan(basePackages = "com.hanu.mediumclone")
 public class MediumCloneApplication {
 
 	public static void main(String[] args) {
